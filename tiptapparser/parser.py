59b6c055c7a1901ce4a08b88885931b2f3655dce
DanielElisenberg <daniel@elisenberg.com>
2020-10-14 23:08:38 +0200

    simple_parsing: naive solution

--- tiptapparser/parser.py
+++ tiptapparser/parser.py
@@ -1,2 +1,29 @@
-def parse():
-    return 1
+def parse(json):
+    html_string = ''
+    for content in json['content']:
+        html_string += parse_content(content)
+    return html_string
+
+
+def parse_content(content):
+    content_type = content['type']
+    prefix = ''
+    postfix = ''
+ 
+    if content_type == 'heading':
+        level = content['attrs']['level']
+        prefix = f'<h{level}>'
+        postfix = f'</h{level}>'
+    elif content_type == 'paragraph':
+        prefix = '<p>'
+        postfix = '</p>'
+    elif content_type == 'text':
+        return content['text']
+    else:
+        return ''
+    
+    inner_content_string = ''
+    for inner_content in content['content']:
+        inner_content_string += parse_content(inner_content)
+    
+    return prefix + inner_content_string + postfix
