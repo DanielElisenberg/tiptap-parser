55bed99514e31fb4a0da866291f36e3a6502f05e
Daniel Elisenberg <33904479+DanielElisenberg@users.noreply.github.com>
2020-10-14 21:23:29 +0200

    initial setup: add download url

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
   license = 'MIT',
   author = 'Daniel Elisenberg',
   url = 'https://github.com/DanielElisenberg/tiptap-parser',
-  download_url = '',
+  download_url = 'https://github.com/DanielElisenberg/tiptap-parser/archive/0.0.1.tar.gz',
   keywords = ['TIPTAP', 'PARSE', 'JSON', 'HTML'],
   install_requires = [],
   classifiers = [
