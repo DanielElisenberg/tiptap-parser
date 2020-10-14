130042934ed10d90f513d8813449f0d28791efd8
Daniel Elisenberg <daniel.elisenberg@byr.oslo.kommune.no>
2020-10-14 21:05:20 +0200

    initial setup

---
+++ setup.py
@@ -0,0 +1,17 @@
+
+setup(
+  name = 'tiptap-parser',
+  packages = ['tiptapparser'],
+  version = '0.0.1',
+  license = 'MIT',
+  author = 'Daniel Elisenberg',
+  url = 'https://github.com/DanielElisenberg/tiptap-parser',
+  download_url = '',
+  keywords = ['TIPTAP', 'PARSE', 'JSON', 'HTML'],
+  install_requires = [],
+  classifiers = [
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Programming Language :: Python :: 3.8',
+  ],
+)
