ec17d55bc35e5c16316b4d933ec1374f99894f5d
DanielElisenberg <daniel@elisenberg.com>
2020-10-14 22:42:41 +0200

    simple_parsing: added simple content tests

--- tests/test_parse.py
+++ tests/test_parse.py
@@ -1,6 +1,6 @@
 from tiptapparser import parse
-
+from tests.testdata import test_data_values as td
 
 def test_parse():
-    assert parse() == 1
+    assert parse(td.simple_content_json) == td.simple_content_html
 
