5d156a03e17ffeb4d559d940ba44cb27b2d2c9e9
Daniel Elisenberg <daniel@elisenberg.com>
2020-10-21 21:40:53 +0200

    try overriding setuptools build

--- setup.py
+++ setup.py
@@ -1,6 +1,12 @@
 import yaml
 
 from distutils.core import setup
+from setuptools.command.build import build as _build
+
+class build(_build):
+    def run(self):
+        subprocess.call(["pip", "install", "pyyaml"])
+        _build.run(self)
 
 with open("version.yml") as version_yml:
     version = yaml.load(version_yml, Loader=yaml.FullLoader)['version']
@@ -14,8 +20,6 @@ setup(
   url='https://github.com/DanielElisenberg/tiptap-parser',
   download_url='https://github.com/DanielElisenberg/tiptap-parser/archive/1.0.2.tar.gz',
   keywords=['TIPTAP', 'PARSE', 'JSON', 'HTML'],
-  setup_requires=['pyyaml'],
-  install_requires=['pyyaml'],
   classifiers=[
     'Development Status :: 4 - Beta',
     'Intended Audience :: Developers',
